4c61fd776a41775f3433b314b3a55f9ebc1d128a
ssyram <liguany126@126.com>
2018-10-14 16:02:56 +0800

    ...

--- src/database/ConnectInfo.java
+++ src/database/ConnectInfo.java
@@ -1,8 +1,25 @@
 package database;
 
+import java.io.*;
+
 class ConnectInfo {
+
     static String driver = "com.mysql.cj.jdbc.Driver";
     static String url = "jdbc:mysql://localhost:3306/business_intelligence_database";
     static String username = "root"; // if necessary, edit here too
-    static String password = "12345678"; //edit here
+    static String password = ""; //edit here
+
+    static {
+        File file = new File("database/password");
+
+        BufferedReader reader;
+        try {
+            reader = new BufferedReader(new FileReader(file));
+            password = reader.readLine();
+            System.out.println(password);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
