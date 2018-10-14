28e576133c101f9dc351186624b54b6fa8fb1f22
ssyram <liguany126@126.com>
2018-10-14 15:46:57 +0800

    ...

--- src/database/ConnectInfo.java
+++ src/database/ConnectInfo.java
@@ -4,5 +4,5 @@ class ConnectInfo {
     static String driver = "com.mysql.cj.jdbc.Driver";
     static String url = "jdbc:mysql://localhost:3306/business_intelligence_database";
     static String username = "root"; // if necessary, edit here too
-    static String password = ""; //edit here
+    static String password = "12345678"; //edit here
 }
